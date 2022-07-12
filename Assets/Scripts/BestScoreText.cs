c4a41ea12c667429bc74d2db83d760fa333a57fd
PollyFox1917 <plukyanova@mail.ru>
2022-07-12 23:05:31 +0700

    Best score

--- Assets/Scripts/BestScoreText.cs
+++ Assets/Scripts/BestScoreText.cs
@@ -8,13 +8,21 @@ public class BestScoreText : MonoBehaviour
 {
     private Text textScore;
     static int highscore;
-    public static int score;
+   
     // Start is called before the first frame update
-    
+
+    public void CheckBestScore(int score)
+    {
+        if (score>PlayerPrefs.GetInt("highscore",0))
+        {
+            PlayerPrefs.SetInt("highscore",score);
+            textScore.text = "Best " + PlayerPrefs.GetString("name", "user") + " score: "+ "Highscore: " + score.ToString();
+        }
+    }
 
     void Start()
     {
-        score = 0;
+      
         highscore = PlayerPrefs.GetInt("highscore",0);
         textScore = GetComponent<Text>();
         textScore.text = "Best " + PlayerPrefs.GetString("name", "user") + " score: "+ "Highscore: " + highscore.ToString();
@@ -23,12 +31,6 @@ public class BestScoreText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (score > highscore)
-        {
-            highscore = score;
-            PlayerPrefs.SetInt("highscore", highscore);
-        }
-
-        score++;
+      
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -14,7 +14,7 @@ public class MainManager : MonoBehaviour
     public GameObject GameOverText;
     
     private bool m_Started = false;
-    private int m_Points;
+    public int m_Points;//well can schet
     
     private bool m_GameOver = false;
 
@@ -70,6 +70,7 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        FindObjectOfType<BestScoreText>().CheckBestScore(m_Points);
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
